bb691bda4d12a0692e7498be9901fe2fcaffb266
houjianjun <474177818@qq.com>
2018-04-28 16:47:38 +0800

    实现：ServletContainer 过滤器与 MVC页面显示

--- RXFrameWork/src/main/java/com/xtwy/common/RestApplication.java
+++ RXFrameWork/src/main/java/com/xtwy/common/RestApplication.java
@@ -1,20 +1,27 @@
 package com.xtwy.common;
 
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
+
 /**
  * 
  * @作者： 侯建军
+ * 
  * @日期：2018年4月24日 下午1:55:06
  * @公司：东软睿道
  * @描述：资源配置类
  */
-public class RestApplication  extends ResourceConfig{
-	public RestApplication(){
-		//配置控制器路径
-        packages("com.xtwy.rest.controller");
-        register(JspMvcFeature.class);
-        //配置模板路径
-        property(JspMvcFeature.TEMPLATE_BASE_PATH, "/WEB-INF/view");
+public class RestApplication extends ResourceConfig {
+	public RestApplication() {
+		// 配置控制器路径
+		packages("com.xtwy.rest.controller");
+		packages("com.xtwy.controller");
+		// 注册JSON转换器
+		register(JacksonJsonProvider.class);
+		// 注册MVC支持
+		register(JspMvcFeature.class);
+		// 配置模板路径,JSP文件是在项目/WEB-INF/view中
+		property(JspMvcFeature.TEMPLATE_BASE_PATH, "/WEB-INF/view");
 	}
 }
--- RXFrameWork/src/main/webapp/WEB-INF/view/index.jsp
+++ RXFrameWork/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,10 +1,13 @@
 <%@page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>MVC测试</title>
 </head>
 <body>
-	Hello,
-	<c:out value="${it.name}" />
+	<!-- 接受控制器hashmap传来的参数，请使用it -->
+	用户：<c:out value="${it.username}" />
 </body>
 </html>
